95f61f3fa67262536ee678477484d1d1dabd8651
karenb9693 <karenbyrne9693@gmail.com>
2019-12-18 19:32:05 +0000

    first draft of bills

---
+++ billmanagement.py
@@ -0,0 +1,79 @@
+import pandas as pd 
+from collections import Counter
+
+def read_bills():
+    return [[col.strip() for col in row.strip().split(',')]
+             for row in open('bills.csv') if len(row) > 1]
+
+def write_bills(bills):
+    bill_file = open('bills.csv', 'w')
+    for bill in bills:
+        bill_file.write(', '.join(bill) + '\n')
+
+def get_message():
+    return 'Hello, Welcome to the Bill Management Company\n' + \
+        '1: View Bills\n2: Insert a Bill\n3: Reports\n4: T&Cs\n5: Exit'
+
+#Function for getting the most popular company
+def get_topComp(bills):
+    for bill in bills:
+        df = pd.DataFrame(bills[0])
+        df[0].value_counts().argmax()
+        #use mode to return the most common company
+        print(df.mode)
+        
+#Function for getting the cred and debit total       
+def get_value_CredDeb(bills):
+   #df = pd.DataFrame(bills)
+   #df.rename(columns={[0]:"Company", [1]: "Account Name", [2]:"Year",[3]:"Month",[4]: "Day", [5]:"Value", [6]:"Type"}, 
+              #   inplace=True)
+            
+   #df.groupby([6]).sum()[5]
+   #print(df)
+    
+    for bill in bills:
+        cred = 0.0
+        deb = 0.0
+        if bill[6] == "credit":
+            cred = bill[5] + cred
+        elif bills[6] == "debit":
+            deb = bills[5] + deb
+        print(cred)
+
+def display_menu():
+    print(get_message())
+
+#Function to print comopany with most amount of bills
+def get_Companies(bills):
+    list1 = list()
+    for bill in bills:
+        list1.append(bill[0])
+    c = Counter((word for word in list1 if word[:1].isupper()))
+    print("Most popular company, count:", c.most_common(1))
+
+def view_bills(bills):
+    for bill in bills:
+        print(bill[0], bill[1], bill[2], bill[3], bill[4], bill[5], bill[6])
+    
+def process_choice(bills):
+    choice = input('Please enter an option:')
+    while choice != '5':
+        if choice == '1':
+            view_bills(bills)
+        elif choice == '2':
+            print("you can insert a new bill here:")
+            write_bills(bills)
+        elif choice == '3':
+            get_Companies(bills)
+        elif choice == '4':
+            print('The terms of the billing management company are:')
+        choice = input('Please enter an option:')
+
+def main():
+    bills = read_bills()
+    display_menu()
+    process_choice(bills)
+    write_bills(bills)
+    
+if __name__ == '__main__':
+    main()
---
+++ billmanagement_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from bill_management import get_message, read_bills, write_bills
+
+class TestBillManagement(unittest.TestCase):
+
+    def test_read_bills(self):
+        bills = read_bills()
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual('credit', bills[19][6])
+
+    def test_write_bills(self):
+        bills = read_bills()
+        write_bills(bills)
+        bills = read_bills()
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual('credit', bills[19][6])
+
+    def test_get_message(self):
+        self.assertEqual('Hello, Welcome to the Bill Management Company\n' + \
+                         '1: View Bills\n2: Insert a Bill\n3: Reports\n4: T&Cs\n5: Exit',
+            get_message())
+
+if __name__ == '__main__':
+    unittest.main()
