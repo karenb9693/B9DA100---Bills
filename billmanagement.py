d2b93d1d705c6d14ac836e12dd249141cafd1f72
karenb9693 <karenbyrne9693@gmail.com>
2019-12-22 12:02:31 +0000

    added function to give average bill price between months

--- billmanagement.py
+++ billmanagement.py
@@ -1,23 +1,19 @@
 
 import pandas as pd 
+#import seaborn as sns
 from collections import Counter
-#from writebills import get_newbill
-#import datetime as dt
 
 def read_bills():
-    bills = pd.read_csv('bills.csv', names=["Company", "Account Name", "Year","Month", "Day", "Value", "Type"])   
-    bills["Value"] = pd.to_numeric(bills['Value'],errors='ignore')
-    bills['Period'] = bills['Year'].astype(str) + "." + bills['Month'].astype(str)+"."+ bills['Day'].astype(str)
+    bills = pd.read_csv('bills.csv', names=["Company", "Account Name", "Year", "Month", "Day", "Value", "Type"])
+    bills["Value"] = pd.to_numeric(bills["Value"])
+    bills['Period'] = bills['Year'].astype(str) +"."+ bills['Month'].astype(str)+"."+ bills['Day'].astype(str)
     bills["Period"] = pd.to_datetime(bills['Period'])
     return bills
-
-#def write_bills(bills1):
- #   #get_newbill()
-  #  billsnew = bills1.append(pd.Series([input("Company: "), input("Account Name: " ), input("Year: "),input("Month: "),
-   #                                       input("Day: "), input("value: ") ,input("Type: ")], index=bills1.columns), ignore_index=True)
-   # billsnew.to_csv('bills.csv')
-   # bills = pd.read_csv('bills.csv')   
-   # return bills
+ 
+#def write_bills(bills):
+ #   bill_file = open('bills.csv', 'w')
+  #  for bill in bills:
+   #     bill_file.write(', '.join(bill) + '\n')
 
 def get_message():
     return 'Hello, Welcome to the Bill Management Company\n' + \
@@ -25,20 +21,21 @@ def get_message():
 
 def get_submenu_message():
     return 'The report options are as follows:' + \
-    '\nx: to go back to main menu\na: information about dataset\nb: credit vs debit \nc: companies \nd: date sort \ne visualisation'
+    '\nx: to go back to main menu\na: information about dataset\nb: credit vs debit \nc: companies \nd: bills sorted'
         
+#Function for getting the cred and debit total       
+def get_value_CredDeb(bills):
+    print('The largest value of credit and debit bills is as follows:',bills.groupby('Type')['Value'].max())
+   
+def get_creddeb_year(bills):
+    print('Value of credit and debit per year is:', bills.groupby(['Year','Type'])['Value'].sum())
+
 def display_menu():
     print(get_message())
     
 def display_submenu():
     print(get_submenu_message())
     
-def get_value_CredDeb(bills):
-    print('The largest value of credit and debit bills is as follows:',bills.groupby('Type')['Value'].max())
-   
-def get_creddeb_year(bills):
-    print('Value of credit and debit per year is:', bills.groupby(['Type','Year'])['Value'].sum())
-
 def display_unique_companies(bills):
     print('# of unique companies:', bills['Company'].nunique())
 
@@ -54,9 +51,12 @@ def get_Companies(bills):
 
 def get_bills_forall_Companies(bills):
     print('Count of bills for all companies is as follows:', Counter(bills['Company']).most_common(100))
-
-def get_avg_time_between_bills(bills):
-    print('Bills sorted in order of the date received:', display_sortedbydate(bills))
+    
+def display_average_spent(bills):
+    #bills = bills[int(input("start range")) <= bills['Month'] <= int(input("end range"))]
+    billstime = bills.loc[(bills['Month'] >= int(input("Start Month")))]
+    billstime = billstime.loc[(billstime['Month'] <= int(input("End Month")))]
+    print("the average bill cost between these months is: ", billstime.sort_values(by='Period')['Value'].mean())
 
 #def get_visualisations(bills):
  #   my_colors = list(['k', 'm', 'b', 'y'])
@@ -65,16 +65,13 @@ def get_avg_time_between_bills(bills):
     #sns.countplot(x="Account Name", hue="Company", data=bills).set_title('Count of bills per customer and company')
     
 def view_bills(bills):
-    print(bills)
+        print(bills)
     
 def process_choice(bills):
     choice = input('Please enter an option:')
     while choice != 'x':
         if choice == '1':
             view_bills(bills)
-            #choice3 = input("do you wish to insert a new bill?")
-            #if choice3 == "1":
-            #    write_bills(bills)
         #elif choice == '2':
          #   print("you can insert a new bill here:")
           #  write_bills(bills)
@@ -96,20 +93,20 @@ def process_choice(bills):
                     break
                 elif choice2 == 'd':
                     display_sortedbydate(bills)
+                    display_average_spent(bills)
                     break
-                #elif choice2 == 'e':
-                    #get_visualisations(bills)
-                 #   break      
+                #elif choice2 == 'd':
+                 #   get_visualisations(bills)
+                  #  break
         elif choice == '4':
             print('The terms of the billing management company are:')
         choice = input('Please enter an option:')
 
 def main():
-    bills = read_bills
-   # bills = write_bills()
+    bills = read_bills()
     display_menu()
     process_choice(bills)
+    #write_bills(bills)
     
 if __name__ == '__main__':
     main()
-
