7823ed058049ef1baab79d241d59c463f07a7452
karenb9693 <57462552+karenb9693@users.noreply.github.com>
2019-12-24 17:56:47 +0000

    Update writebills.py

--- writebills.py
+++ writebills.py
@@ -1,41 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 21 22:05:38 2019
 
-@author: karenbyrne
-"""
 import pandas as pd
 import datetime
 
 def get_newbill():
     now = datetime.datetime.now()
     
-    choice_year = input("Enter Year:")
+    choice_year = input("Enter Year: ")
     choice_year = int(choice_year)
     
     while(choice_year>now.year):
         print("This is not a valid input")
+        print("Please enter a year up until the current year")
+        choice_year = input("Enter Year: ")
+        Year = choice_year
         break
     else:
         Year = choice_year
     
-    choice_month = input("Enter Month:")
+    choice_month = input("Enter Month: ")
     choice_month = int(choice_month)
-    if 1 <= choice_month <= 12:
+    if 0 < choice_month <= 12:
         Month = choice_month
     else:
         print("This is not a valid input")
     
     choice_day = input("Enter Day: ")
     choice_day = int(choice_day)
-    if choice_month == 2 & (1 <= choice_day <= 28):
+    if (choice_month == 2) & (1 <= choice_day <= 28):
         Day = choice_day
-    elif (choice_month == 4 or choice_month == 6 or choice_month == 9 or choice_month == 11) & (1 <= choice_day <= 30):
+    elif (choice_month == 4 or choice_month == 6 or choice_month == 9 or 
+          choice_month == 11) & (0 < choice_day <= 30):
         Day = choice_day
-    elif (choice_year % 4 == 0 & choice_year % 100 != 0 & choice_year % 400 == 0) & (choice_month == 2) & (1 <= choice_day <= 29):
+    elif (choice_year % 4 == 0 & choice_year % 100 != 0 & 
+          choice_year % 400 == 0) & (choice_month == 2) & (0 < choice_day <= 29):
         Day = choice_day
-    elif (choice_month == 1 or choice_month == 3 or choice_month == 5 or choice_month == 7 or choice_month == 8 or choice_month == 10 or choice_month == 11) & (1 <= choice_day <= 31):
+    elif (choice_month == 1 or choice_month == 3 or choice_month == 5 or 
+          choice_month == 7 or choice_month == 8 or choice_month == 10 or 
+          choice_month == 12) & (0 < choice_day <= 31):
         Day = choice_day
     else:
         print("This is not a valid input")
@@ -50,7 +51,7 @@ def get_newbill():
     elif choice_debcred == 2:
         Type = " debit"
         
-    billsold = pd.read_csv('bills.csv', names=["Company", "Account Name", "Year","Month", "Day", "Value", "Type"])
+    billsold = pd.read_csv('bills.csv')
     billsnew = billsold.append(pd.Series([input("Company: "), input("Account Name: " ), Year,Month,Day, choice_value ,Type], index=billsold.columns), ignore_index=True)
-    billsnew.to_csv('bills.csv')
+    billsnew.to_csv('bills.csv', index=False)
     return billsnew
