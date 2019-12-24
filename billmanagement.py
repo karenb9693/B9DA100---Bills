d0f70ce5b5f9be3ac68e135f3872b42766b9cab4
karenb9693 <karenbyrne9693@gmail.com>
2019-12-24 13:25:13 +0000

    bills.csv extended

--- billmanagement.py
+++ billmanagement.py
@@ -5,9 +5,11 @@ from collections import Counter
 from writebills import get_newbill
 
 def read_bills():
-    bills = pd.read_csv('bills.csv', names=["Company", "Account Name", "Year", "Month", "Day", "Value", "Type"])
+    bills = pd.read_csv('bills.csv', names=["Company", "Account Name", "Year",\
+                                            "Month", "Day", "Value", "Type"])
     bills["Value"] = pd.to_numeric(bills["Value"])
-    bills['Period'] = bills['Year'].astype(str) +"."+ bills['Month'].astype(str)+"."+ bills['Day'].astype(str)
+    bills['Period'] = bills['Year'].astype(str) +"."+ \
+        bills['Month'].astype(str)+"."+ bills['Day'].astype(str)
     bills["Period"] = pd.to_datetime(bills['Period'])
     return bills
  
@@ -20,14 +22,18 @@ def get_message():
 
 def get_submenu_message():
     return 'The report options are as follows:' + \
-    '\nx: Exit to main menu\na: Count of bills/companies \nb: Credit vs Debit \nc: Most popular \nd: Bills sorted \ne: Average spent \nf: Average time between bill'
+    '\nx: Exit to main menu\na: Count of bills/companies \nb: Credit vs Debit \
+    \nc: Most popular \nd: Bills sorted \ne: Average spent \
+    \nf: Average time between bill'
         
 #Function for getting the cred and debit total       
 def get_value_CredDeb(bills):
-    print('The largest value of credit and debit bills is as follows:',bills.groupby('Type')['Value'].max())
+    print('The largest value of credit and debit bills is as follows:',
+          bills.groupby('Type')['Value'].max())
    
 def get_creddeb_year(bills):
-    print('Value of credit and debit per year is:', bills.groupby(['Year','Type'])['Value'].sum())
+    print('Value of credit and debit per year is:', \
+          bills.groupby(['Year','Type'])['Value'].sum())
 
 def display_menu():
     print(get_message())
@@ -51,23 +57,29 @@ def get_Companies(bills):
     print('most popular company is:', Counter(bills['Company']).most_common(1))
 
 def get_bills_forall_Companies(bills):
-    print('Count of bills for all companies is as follows:', Counter(bills['Company']).most_common(100))
+    print('Count of bills for all companies is as follows:', \
+          Counter(bills['Company']).most_common(100))
     
 def display_average_spent(bills):
     billstime = bills.loc[(bills['Month'] >= int(input("Start Month ")))]
     billstime = billstime.loc[(billstime['Month'] <= int(input("End Month ")))]
-    print("the average bill cost between these months is: ", billstime.sort_values(by='Period')['Value'].mean())
+    print("the average bill cost between these months is: ", \
+          billstime.sort_values(by='Period')['Value'].mean())
     
 def avg_time_between_bills(bills):
     bills = bills.sort_values(by='Period')
-    print("Avg time between a bill is: ",(bills['Period'].diff().sum().days)/(bills['Company'].count()), "days")
+    print("Avg time between a bill is: ",\
+          (round(bills['Period'].diff().sum().days)/(bills['Company'].count())), "days")
     return (bills['Period'].diff().sum().days)/(bills['Company'].count())
 
 def get_visualisations(bills):
     my_colors = list(['k', 'm', 'b', 'y'])
-    bills['Company'].value_counts().plot.barh(color=my_colors).set_title("Count of bills per customer")
-    sns.boxplot(bills['Year'], bills['Value']).set_title("Value of bills per year")
-    sns.countplot(x="Account Name", hue="Company", data=bills).set_title('Count of bills per customer and company')
+    bills['Company'].value_counts().plot.barh(color=my_colors).\
+    set_title("Count of bills per customer")
+    sns.boxplot(bills['Year'], bills['Value']).\
+    set_title("Value of bills per year")
+    sns.countplot(x="Account Name", hue="Company", data=bills).\
+    set_title('Count of bills per customer and company')
     
 def view_bills(bills):
         print(bills)
@@ -78,13 +90,14 @@ def process_choice(bills):
         if choice == '1':
             view_bills(bills)
         elif choice == '2':
-            print("you can insert a new bill here:")
+            print("you can insert a new bill here: ")
             bills.iloc[0:0]
             write_bills()
-            print("To provide an analysis including the new reports added you must exit and re-enter the program")
+            print("To provide an analysis including the new reports"
+                  " added you must exit and re-enter the program")
         elif choice == '3':
             display_submenu()
-            choice2 = input('Please select requested report:')
+            choice2 = input('Please select requested report: ')
             while choice2 != 'x':
                 if choice2 == 'a':
                     display_bill_count(bills)
